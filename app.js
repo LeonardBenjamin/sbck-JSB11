9da1544056295ab88b190face51fb5cac6b4eb93
LeonardBenjamin <136946134+LeonardBenjamin@users.noreply.github.com>
2023-08-11 11:05:28 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function news() {
-  location.replace("https://baotanghochiminh.vn/le-ky-ket-thoa-thuan-hop-tac-van-hoa-va-khoa-hoc-giai-doan-2023-2025-giua-bao-tang-ho-chi-minh-viet-nam-va-bao-tang-lich-su-chinh-tri-quoc-gia-nga.htm")
-}
-
-function bifacebook() {
-  location.replace("https://www.facebook.com/tinamiarosaleen/")
-}
